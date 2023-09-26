034ac6a3f2b3e2efca83bbb03959a92d51801058
K <greckii@Ks-MacBook-Pro.local>
2023-09-26 07:04:15 +0300

    Fix tests

--- tests/html.spec.ts
+++ tests/html.spec.ts
@@ -1,10 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const html = fs.readFileSync(
-  path.resolve(__dirname, '../src/index.html'),
-  'utf8',
-);
+const html = fs.readFileSync(path.resolve(__dirname, '../src/index.html'), 'utf8');
 beforeEach(() => {
   document.documentElement.innerHTML = html.toString();
 });
@@ -16,7 +13,7 @@ describe('Inputs have valid attributes', () => {
     expect(input?.step).toBe('10');
     expect(input?.min).toBe('70');
     expect(input?.max).toBe('140');
-    expect(input?.value).toBe('70');
+    expect(input?.value).toBe('80');
   });
 
   test('Input with a range of temperatures from -10º to 40º using steps of 10º', () => {
@@ -24,23 +21,17 @@ describe('Inputs have valid attributes', () => {
     expect(input?.step).toBe('10');
     expect(input?.min).toBe('-10');
     expect(input?.max).toBe('40');
-    expect(input?.value).toBe('-10');
+    expect(input?.value).toBe('10');
   });
 
   test('A toggle for air conditioner/heating system', () => {
-    const input = document.querySelector<HTMLInputElement>(
-      'input#ac[type="checkbox"]',
-    );
+    const input = document.querySelector<HTMLInputElement>('input#ac[type="checkbox"]');
     expect(input).not.toBeNull();
   });
 
   test('A wheel size selection with 19" and 21".', () => {
-    const input19 = document.querySelector<HTMLInputElement>(
-      'input[type="radio"][value="19"]',
-    );
-    const input21 = document.querySelector<HTMLInputElement>(
-      'input[type="radio"][value="21"]',
-    );
+    const input19 = document.querySelector<HTMLInputElement>('input[type="radio"][value="19"]');
+    const input21 = document.querySelector<HTMLInputElement>('input[type="radio"][value="21"]');
     expect(input19).not.toBeNull();
     expect(input21).not.toBeNull();
   });
@@ -63,13 +54,9 @@ describe('HTML template is valid', () => {
     expect(document.querySelector('.main')).not.toBeNull();
     expect(document.querySelector('.intro')).not.toBeNull();
 
-    expect(
-      document.querySelector('.intro__title._observed-fade-in'),
-    ).not.toBeNull();
+    expect(document.querySelector('.intro__title._observed-fade-in')).not.toBeNull();
 
-    expect(
-      document.querySelector('.intro__subtitle._observed-fade-in'),
-    ).not.toBeNull();
+    expect(document.querySelector('.intro__subtitle._observed-fade-in')).not.toBeNull();
     expect(document.querySelector(`a[href="#calculator"]`)).not.toBeNull();
 
     expect(document.querySelector(`.scroll-button__circle img`)).not.toBeNull();
@@ -86,24 +73,14 @@ describe('HTML template is valid', () => {
     expect(document.querySelector(`.calculation__disclaimer`)).not.toBeNull();
     expect(document.querySelectorAll(`.calculator__result`).length).toBe(2);
 
-    expect(
-      document.querySelectorAll(`.calculator__fields .calculator__field-group`)
-        .length,
-    ).toBe(3);
+    expect(document.querySelectorAll(`.calculator__fields .calculator__field-group`).length).toBe(3);
   });
 
   test('Page has a valid footer', () => {
-    expect(
-      document.querySelectorAll('.footer .footer__content .footer__group')
-        .length,
-    ).toBe(4);
+    expect(document.querySelectorAll('.footer .footer__content .footer__group').length).toBe(4);
   });
 
   test('Page has a cursor animation overlay', () => {
-    expect(
-      document.querySelectorAll(
-        '.overlay .cursor .cursor__circle[aria-hidden="true"]',
-      ),
-    ).not.toBeNull();
+    expect(document.querySelectorAll('.overlay .cursor .cursor__circle[aria-hidden="true"]')).not.toBeNull();
   });
 });
